051b4c2f6eae060af51a1feecc1ef963d4b17aff
Boris Sterligov <bsterligov@gmail.com>
2024-10-06 11:02:41 +0200

    mandelbrot

--- chapter-2/console/src/main.rs
+++ chapter-2/console/src/main.rs
@@ -21,7 +21,7 @@ fn main() {
     println!("The greatest common divisor of {:?} is {}", numbers, d);
 }
 
-fn gcp(mut m: u64, mut n:u64) -> u64
+pub fn gcp(mut m: u64, mut n:u64) -> u64
 {
     assert!(n !=0 && m != 0);
     while m != 0 {
